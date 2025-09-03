710ad4d850c84c3462495bd3e8e2fc35479d1d80
levi-45 <levi45@hotmail.co.uk>
2025-09-03 17:59:28 +0100

    .

--- ipkinstall.sh
+++ ipkinstall.sh
@@ -2,15 +2,15 @@
 #### "***********************************************"
 #### "*Levi45 https://satellite-forum.com/index.php *"
 #### "***********************************************"
-# Download fresh copy
+# Downloading
 echo "Downloading OscamEmu..."
 wget -q --no-check-certificate "https://raw.githubusercontent.com/levi-45/Levi45Emulator/main/levi45_all.ipk" -O /tmp/levi45_all.ipk
 
-# Force install (ignore version checks)
+# Force installation
 echo "Installing..."
 opkg install --force-reinstall --force-overwrite /tmp/levi45_all.ipk
 
-# Cleanup
+# Cleanup tmp
 rm -f /tmp/levi45_all.ipk
 echo "Please Restart GUI..."
 exit
