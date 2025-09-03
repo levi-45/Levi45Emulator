84f2070c9f520b8192136cff53c16d3bc1353cdd
levi-45 <levi45@hotmail.co.uk>
2025-09-03 17:21:35 +0100

    .

---
+++ ipkinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#### "***********************************************"
+#### "*Levi45 https://satellite-forum.com/index.php *"
+#### "***********************************************"
+# Download fresh copy
+echo "Downloading OscamEmu..."
+wget -q --no-check-certificate "https://raw.githubusercontent.com/levi-45/Multicam/main/Cams_ipk/All/enigma2-plugin-softcams-oscam-emu-levi45_*-*_all.ipk" -O /tmp/OscamEmu.ipk
+
+# Force install (ignore version checks)
+echo "Installing..."
+opkg install --force-reinstall --force-overwrite /tmp/OscamEmu.ipk
+
+# Cleanup
+rm -f /tmp/OscamEmu.ipk
+echo "Please Restart GUI..."
+exit
